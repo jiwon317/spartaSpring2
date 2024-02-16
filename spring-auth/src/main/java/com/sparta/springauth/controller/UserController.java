e4f46d1ba5070ba9c854c2ef7b0dfe78b73e6c82
jiwon317 <chlghqlt12@naver.com>
2024-02-16 13:04:05 +0900

    필터

--- spring-auth/src/main/java/com/sparta/springauth/controller/UserController.java
+++ spring-auth/src/main/java/com/sparta/springauth/controller/UserController.java
@@ -13,38 +13,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api")
 public class UserController {
 
-    private final UserService userService;
-    private LoginRequestDto requestDto;
-    private HttpServletResponse res;
+  private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
 
-    @GetMapping("/user/login-page")
-    public String loginPage() {
-        return "login";
-    }
+  @GetMapping("/user/login-page")
+  public String loginPage() {
+    return "login";
+  }
 
-    @GetMapping("/user/signup")
-    public String signupPage() {
-        return "signup";
-    }
+  @GetMapping("/user/signup")
+  public String signupPage() {
+    return "signup";
+  }
 
-    @PostMapping("/user/signup")
-    public String signup(SignupRequestDto requestDto) {
-        userService.signup(requestDto);
-        return "redirect:/api/user/login-page";
-    }
+  @PostMapping("/user/signup")
+  public String signup(SignupRequestDto requestDto) {
+    userService.signup((requestDto));
 
-    @PostMapping("/user/login")
-    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
-        try {
-            userService.login(requestDto, res);
-        } catch (Exception e) {
-            return "redirect:/api/user/login-page?error";
-        }
+    return "redirect:/api/user/login-page";
+  }
 
-        return "redirect:/";
-    }
+  @PostMapping("/user/login")
+  public String login(LoginRequestDto requestDto, HttpServletResponse res) {
+      try {
+          userService.login(requestDto, res);
+      } catch (Exception e) {
+          return "redirect:/api/user/login-page?error";
+      }
+
+      return "redirect:/";
+  }
 }
--- spring-auth/src/main/java/com/sparta/springauth/service/UserService.java
+++ spring-auth/src/main/java/com/sparta/springauth/service/UserService.java
@@ -65,7 +65,7 @@ public class UserService {
 
         // 사용자 확인
         User user = userRepository.findByUsername(username).orElseThrow(
-                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+            () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
         );
 
         // 비밀번호 확인
